4529edd3224ca3d94fdc759ce4997626c1cacb08
TOURNOUX Thomas <thomasdevgif@gmail.com>
2018-03-25 03:15:49 +0200

    Expand arrow in parralax section

--- cloudev.fr/components/profile/profile.js
+++ cloudev.fr/components/profile/profile.js
@@ -1,8 +1,5 @@
-var elem = document.querySelector('.carousel');
-var instance = M.Carousel.init(elem, null);
-
 $( document ).ready(function() {
   // Load html pages
   $("#SkillsComponent").load("cloudev.fr/components/profile/skills/skills.html");
-  $('.carousel').carousel();
+  $("#DiplomasComponent").load("cloudev.fr/components/profile/diplomas/diplomas.html");
 });
