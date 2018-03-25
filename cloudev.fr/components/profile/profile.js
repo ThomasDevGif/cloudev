a49f079a5e54ccf19b8016998ff7f9e8a85b0ac0
TOURNOUX Thomas <thomasdevgif@gmail.com>
2018-03-25 21:53:34 +0200

    Add experiences

--- cloudev.fr/components/profile/profile.js
+++ cloudev.fr/components/profile/profile.js
@@ -2,4 +2,5 @@ $( document ).ready(function() {
   // Load html pages
   $("#SkillsComponent").load("cloudev.fr/components/profile/skills/skills.html");
   $("#DiplomasComponent").load("cloudev.fr/components/profile/diplomas/diplomas.html");
+  $("#ExperiencesComponent").load("cloudev.fr/components/profile/experiences/experiences.html");
 });
--- cloudev.fr/images/social-icons/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/ameyraut/pen/yfzog.
-
- Font Awesome Brand and social Icons with brand color
