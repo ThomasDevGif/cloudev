c3ab07aa9544a82677365ca16032f2ce900fdc0c
TOURNOUX Thomas <thomasdevgif@gmail.com>
2018-03-25 03:00:40 +0200

    Add formations and schools

--- cloudev.fr/components/profile/profile.js
+++ cloudev.fr/components/profile/profile.js
@@ -1,3 +1,8 @@
+var elem = document.querySelector('.carousel');
+var instance = M.Carousel.init(elem, null);
+
 $( document ).ready(function() {
-  
+  // Load html pages
+  $("#SkillsComponent").load("cloudev.fr/components/profile/skills/skills.html");
+  $('.carousel').carousel();
 });
