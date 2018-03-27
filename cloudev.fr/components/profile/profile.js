08e5c3e1d06ed3c8178ea60e7ea1e172bef46bca
TOURNOUX Thomas <thomasdevgif@gmail.com>
2018-03-27 22:21:04 +0200

    Add about me section

--- cloudev.fr/components/profile/profile.js
+++ cloudev.fr/components/profile/profile.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
   // Load html pages
+  $("#ProfileCardComponent").load("cloudev.fr/components/profile/profile-card/profile-card.html");
+  $("#AboutComponent").load("cloudev.fr/components/profile/about/about.html");
   $("#SkillsComponent").load("cloudev.fr/components/profile/skills/skills.html");
   $("#DiplomasComponent").load("cloudev.fr/components/profile/diplomas/diplomas.html");
   $("#ExperiencesComponent").load("cloudev.fr/components/profile/experiences/experiences.html");
