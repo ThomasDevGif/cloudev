c26748e68e44f15660a0845051fd072ee003ded1
TOURNOUX Thomas <thomasdev.gif@gmail.com>
2018-03-29 10:24:03 +0200

    Set more width in container

--- cloudev.fr/js/index.js
+++ cloudev.fr/js/index.js
@@ -1,15 +1,15 @@
-$( document ).ready(function() {
-  // Load html pages
-  $("#NavbarComponent").load("cloudev.fr/components/navbar/navbar.html");
-  $("#SidenavComponent").load("cloudev.fr/components/sidenav/sidenav.html");
-  $("#ParallaxHomeComponent").load("cloudev.fr/components/parallax-home/parallax-home.html");
-  $("#ProfileCardComponent").load("cloudev.fr/components/profile/profile-card/profile-card.html");
-  $("#AboutComponent").load("cloudev.fr/components/profile/about/about.html");
-  $("#SkillsComponent").load("cloudev.fr/components/profile/skills/skills.html");
-  $("#DiplomasComponent").load("cloudev.fr/components/profile/diplomas/diplomas.html");
-  $("#ExperiencesComponent").load("cloudev.fr/components/profile/experiences/experiences.html");
-  $("#MySitesComponent").load("cloudev.fr/components/my-sites/my-sites.html");
+// Load html pages
+$("#NavbarComponent").load("cloudev.fr/components/navbar/navbar.html");
+$("#SidenavComponent").load("cloudev.fr/components/sidenav/sidenav.html");
+$("#ParallaxHomeComponent").load("cloudev.fr/components/parallax-home/parallax-home.html");
+$("#ProfileCardComponent").load("cloudev.fr/components/profile/profile-card/profile-card.html");
+$("#AboutComponent").load("cloudev.fr/components/profile/about/about.html");
+$("#SkillsComponent").load("cloudev.fr/components/profile/skills/skills.html");
+$("#DiplomasComponent").load("cloudev.fr/components/profile/diplomas/diplomas.html");
+$("#ExperiencesComponent").load("cloudev.fr/components/profile/experiences/experiences.html");
+$("#MySitesComponent").load("cloudev.fr/components/my-sites/my-sites.html");
 
+$( document ).ready(function() {
   // Scroll to profile
   var profile = $('#profile');
   $('#ParallaxHomeComponent').click(function() {
@@ -19,7 +19,6 @@ $( document ).ready(function() {
   // Scroll to top
   var body = $("html, body");
   $('#fabToTop').click(function() {
-    body.stop().animate({scrollTop: 0}, 500, 'swing');
+    body.stop().animate({scrollTop: 0}, 700, 'swing');
   })
-
 });
