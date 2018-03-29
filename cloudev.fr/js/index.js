7514887852baf3566c2877c2f3a8ddc944e94bf4
TOURNOUX Thomas <thomasdev.gif@gmail.com>
2018-03-29 09:43:20 +0200

    Move all load functinos to index.js

--- cloudev.fr/js/index.js
+++ cloudev.fr/js/index.js
@@ -1,8 +1,12 @@
 $( document ).ready(function() {
   // Load html pages
+  $("#NavbarComponent").load("cloudev.fr/components/navbar/navbar.html");
+  $("#SidenavComponent").load("cloudev.fr/components/sidenav/sidenav.html");
+  $("#ParallaxHomeComponent").load("cloudev.fr/components/parallax-home/parallax-home.html");
   $("#ProfileCardComponent").load("cloudev.fr/components/profile/profile-card/profile-card.html");
   $("#AboutComponent").load("cloudev.fr/components/profile/about/about.html");
   $("#SkillsComponent").load("cloudev.fr/components/profile/skills/skills.html");
   $("#DiplomasComponent").load("cloudev.fr/components/profile/diplomas/diplomas.html");
   $("#ExperiencesComponent").load("cloudev.fr/components/profile/experiences/experiences.html");
+  $("#MySitesComponent").load("cloudev.fr/components/my-sites/my-sites.html");
 });
